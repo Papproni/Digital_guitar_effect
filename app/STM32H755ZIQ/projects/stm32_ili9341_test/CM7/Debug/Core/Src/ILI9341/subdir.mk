3684646fc084cec34b0d41650cd8d9c2aa0dcc70
pappr <pappr@DESKTOP-RQC28UQ.local>
2023-10-22 15:55:53 +0200

    ILI9341 testing project added for stm32h755ziq dev board usign spi3

---
+++ app/STM32H755ZIQ/projects/stm32_ili9341_test/CM7/Debug/Core/Src/ILI9341/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/ILI9341/ILI9341_GFX.c \
+../Core/Src/ILI9341/ILI9341_STM32_Driver.c 
+
+OBJS += \
+./Core/Src/ILI9341/ILI9341_GFX.o \
+./Core/Src/ILI9341/ILI9341_STM32_Driver.o 
+
+C_DEPS += \
+./Core/Src/ILI9341/ILI9341_GFX.d \
+./Core/Src/ILI9341/ILI9341_STM32_Driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/ILI9341/%.o Core/Src/ILI9341/%.su: ../Core/Src/ILI9341/%.c Core/Src/ILI9341/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H755xx -c -I../Core/Inc -I../../Drivers/STM32H7xx_HAL_Driver/Inc -I../../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/BSP/Components/ili9341 -I../../Drivers/BSP/Components/Common -I"C:/Projects/STM32H755/display_workspace/stm32_ili9341_test/CM7/Core/Src/ILI9341" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src-2f-ILI9341
+
+clean-Core-2f-Src-2f-ILI9341:
+	-$(RM) ./Core/Src/ILI9341/ILI9341_GFX.d ./Core/Src/ILI9341/ILI9341_GFX.o ./Core/Src/ILI9341/ILI9341_GFX.su ./Core/Src/ILI9341/ILI9341_STM32_Driver.d ./Core/Src/ILI9341/ILI9341_STM32_Driver.o ./Core/Src/ILI9341/ILI9341_STM32_Driver.su
+
+.PHONY: clean-Core-2f-Src-2f-ILI9341
+
---
+++ app/STM32H755ZIQ/projects/stm32_ili9341_test/CM7/Debug/DISPLAY/Target/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../DISPLAY/Target/lcd_io.c \
+../DISPLAY/Target/lcd_os.c 
+
+OBJS += \
+./DISPLAY/Target/lcd_io.o \
+./DISPLAY/Target/lcd_os.o 
+
+C_DEPS += \
+./DISPLAY/Target/lcd_io.d \
+./DISPLAY/Target/lcd_os.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+DISPLAY/Target/%.o DISPLAY/Target/%.su: ../DISPLAY/Target/%.c DISPLAY/Target/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H755xx -c -I../Core/Inc -I../../Drivers/STM32H7xx_HAL_Driver/Inc -I../../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../../Drivers/CMSIS/Include -I../DISPLAY/Target -I../../Drivers/BSP/Components/ili9341 -I../../Drivers/BSP/Components/Common -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-DISPLAY-2f-Target
+
+clean-DISPLAY-2f-Target:
+	-$(RM) ./DISPLAY/Target/lcd_io.d ./DISPLAY/Target/lcd_io.o ./DISPLAY/Target/lcd_io.su ./DISPLAY/Target/lcd_os.d ./DISPLAY/Target/lcd_os.o ./DISPLAY/Target/lcd_os.su
+
+.PHONY: clean-DISPLAY-2f-Target
+
